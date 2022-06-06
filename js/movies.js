ba7f1205828c995e2879799e78ae321c853367d8
Shane Banks <shane.banks02@gmail.com>
2022-06-06 16:50:28 -0500

    more structure

--- js/movies.js
+++ js/movies.js
@@ -1,5 +1,24 @@
 "use strict";
 (function () {
 
+    // This projects API: https://aromatic-subsequent-sun.glitch.me/movies
+    // Poster API in keys file
+
+    const getMovies = () => {
+        const URL = "https://aromatic-subsequent-sun.glitch.me/movies";
+        return fetch(URL)
+            .then(res => res.json())
+            .then(results => console.log(results))
+            .catch(error => console.error(error))
+    }
+    getMovies()
+
+
+
+
+
+
+
+
 
 }())
