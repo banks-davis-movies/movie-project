5b501883e5e9ccb8d04b4f24356898e01bc18125
Shane Banks <shane.banks02@gmail.com>
2022-06-07 08:47:45 -0500

    Scripts created. need some adjustments

--- js/movies.js
+++ js/movies.js
@@ -1,9 +1,11 @@
 "use strict";
 (function () {
 
-    // This projects API: https://aromatic-subsequent-sun.glitch.me/movies
+    // This project's API: https://aromatic-subsequent-sun.glitch.me/movies
     // Poster API in keys file
 
+
+    //load available movies. Needs edit to add data to page
     const getMovies = () => {
         const URL = "https://aromatic-subsequent-sun.glitch.me/movies";
         return fetch(URL)
@@ -11,14 +13,77 @@
             .then(results => console.log(results))
             .catch(error => console.error(error))
     }
-    getMovies()
+    //getMovies()
+
+
+//pulls movie by ID number
+    const SelectById= (id) => {
+        //const URL2 = `https://aromatic-subsequent-sun.glitch.me/movies/${id}`;
+        return fetch(`${URL}/${id}`).then(res => res.json()).then(res => console.log(res));
+    }
+    SelectById(260);
 
 
 
+    const addMovie = (movieObj) => {
+        let options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(songObj)
+        }
+        return fetch(URL, options).then(res => res.json()).then(result => console.log("You've successfully created a new song!", result))
+    }
 
 
+    //builds movie posters. Needs work
+    const buildMovie = () => {
+        getMovies().then((data) => {
+            let posters = data.map(movie => {
+                return `
+            <div>
+            <h3>Title: ${movie.title}</h3>
+            <p>Artist: ${movie.director}</p>
+            <p>Genre: ${movie.genre}</p>
+            <p>Rating: ${movie.rating}</p>
+            <button data-id="${movie.id}">Edit</button>
+            <button data-id="${movie.id}">Delete</button>
+            </div>
+            `
+            })
+            document.getElementById("ENTERLOCATION").innerHTML = posters.join("");
+        })
+    }
+    buildMovie();
 
 
+//add movie
+    document.getElementById("ENTERLOCATION").addEventListener("click", function (e) {
+        e.preventDefault();
+        let newMovie = {
+            title: document.getElementById("title").value,
+            artist: document.getElementById("artist").value
+        }
+        addMovie(newMovie).then((res) => {
+            console.log(res)
+            buildMovie()
+        })
+    })
+
+
+//delete a movie
+    const deleteMovie = (id) => {
+        let options = {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        return fetch(`${URL}/${id}`, options).then(() => console.log("The song has been deleted successfully")).then(buildMovie)
+    }
+
+    //deleteMovie();
 
 
 }())
