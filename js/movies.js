3adb497b3a4ef5187cf9a5b06e7850bbfeb067d6
s-banks <shroud2@hotmail.com>
2022-06-07 19:43:14 -0500

    separated and refactored api call and card builder for future flexability

--- js/movies.js
+++ js/movies.js
@@ -6,44 +6,26 @@
 
     const URL = "https://aromatic-subsequent-sun.glitch.me/movies";
 
-    //load available movies.
-    const getMovies = () => {
-        return fetch(URL)
+    //load available movies. Can build all the search functionality into this as well
+    const callMovies = () => {
+        fetch(URL)
             .then(res => res.json())
+            .then(movies => {
+                console.log(movies);
+                showMovies(movies);
+                //add search features here to leverage the current api call
+            })
     }
+    callMovies()
 
 
-//     const addMovie = (movieObj) => {
-//         let options = {
-//             method: "POST",
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(songObj)
-//         }
-//         return fetch(URL, options).then(res => res.json()).then(result => console.log("You've added a movie", result))
-//     }
-
-    //add movie
-    //document.getElementById("ENTERLOCATION").addEventListener("click", function (e) {
-    //     e.preventDefault();
-    //     let newMovie = {
-    //         title: document.getElementById("title").value,
-    //         artist: document.getElementById("artist").value
-    //     }
-    //     addMovie(newMovie).then((res) => {
-    //         console.log(res)
-    //         buildMovie()
-    //     })
-    // })
 
 
-    //builds movie posters.
-    const buildMovie = () => {
-        //console.log('building movies')
-            getMovies().then((data) => {
-                let posters = data.map(movie => {
-                    return `
+//build movie cards
+    function showMovies(movies) {
+        movies.forEach(movie => {
+            if (movie.title !== undefined) {
+            $("#movie-display").append(`
                 <div class="card mx-auto flip-card col-3">
             <div class="flip-card-inner">
                 <div class="flip-card-front">
@@ -57,14 +39,43 @@
                     <button data-id="${movie.id}">Delete</button>
                 </div>
             </div>
-        </div>
-            `
-                })
-                //console.log(posters)
-                document.querySelector("#movie-display").innerHTML = posters.join("");
-            })
+        </div>`);
+            }
+        });
     }
-    buildMovie();
+
+
+
+
+
+//rough draft code below. will need finished forms to be useful
+
+
+//     const addMovie = (movieObj) => {
+//         let options = {
+//             method: "POST",
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(songObj)
+//         }
+//         return fetch(URL, options).then(res => res.json()).then(result => console.log("You've added a movie", result))
+//     }
+
+//add movie click event
+//document.getElementById("ENTERLOCATION").addEventListener("click", function (e) {
+//     e.preventDefault();
+//     let newMovie = {
+//         title: document.getElementById("title").value,
+//         artist: document.getElementById("artist").value
+//     }
+//     addMovie(newMovie).then((res) => {
+//         console.log(res)
+//         buildMovie()
+//     })
+// })
+
+
 
 // Edit Movies
 //     const changeMovie = (movie) => {
@@ -81,36 +92,7 @@
 //         id: 1,
 //         title: "TNT",
 //     }
-    //changeMovie(updatedMovie);
-
-
-
-
-
-//Add a movie
-    const addMovie = (movieObj) => {
-        let options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(movieObj)
-        }
-        return fetch(URL, options).then(res => res.json()).then(result => console.log("You've added a movie", result))
-    }
-
-    //add movie
-    //document.getElementById("ENTERLOCATION").addEventListener("click", function (e) {
-    //     e.preventDefault();
-    //     let newMovie = {
-    //         title: document.getElementById("title").value,
-    //         artist: document.getElementById("artist").value
-    //     }
-    //     addMovie(newMovie).then((res) => {
-    //         console.log(res)
-    //         buildMovie()
-    //     })
-    // })
+//changeMovie(updatedMovie);
 
 
 
@@ -125,61 +107,59 @@
 //         return fetch(`${URL}/${id}`, options).then(() => console.log("The song has been deleted successfully")).then(buildMovie)
 //     }
 
-    //deleteMovie();
+//deleteMovie();
 //console.log(`${URL}/${id}`)
 
 
 
-
-    function starRating(num) {
-        if (num === 1) {
-            return `
+//convert rating in API call to a star display
+function starRating(num) {
+    if (num === 1) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-        }
-         else if (num === 2) {
-                return `
+    }
+    else if (num === 2) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-            }
-            else if (num === 3) {
-                    return `
+    }
+    else if (num === 3) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-                }
-                 else if (num === 4) {
-                        return `
+    }
+    else if (num === 4) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                     `
-                    }
-                     else if (num === 5) {
-                            return `
+    }
+    else if (num === 5) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                     `
-                        }
-                    }
-
-    //console.log(starRating(2));
+    }
+}
 
 
 }())
