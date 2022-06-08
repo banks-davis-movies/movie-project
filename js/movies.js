9bacdb7287703b35ac80c657eaa24b0b6ad1754c
Shane Banks <shane.banks02@gmail.com>
2022-06-08 15:11:04 -0500

    Search functionality complete

--- js/movies.js
+++ js/movies.js
@@ -11,10 +11,8 @@
         fetch(URL)
             .then(res => res.json())
             .then(movies => {
-                console.log(movies);
+                //console.log(movies);
                 showMovies(movies);
-                //add search features here to leverage the current api call
-                // console.log($(".delete"));
                 $(".delete").click(function () {
                     deletion($(this).attr("data-id")).then(callMovies);
                 })
@@ -48,7 +46,7 @@
                 <div class="flip-card-front">
                     <img src="${movie.poster}" alt="poster" class="poster">
                 </div>
-                <div class="card-body flip-card-back">
+                <div class="card-body flip-card-back" id="pData">
                     <h3>Title: ${movie.title}</h3>
                     <p>Rating: ${starRating(parseInt(movie.rating))}</p>
                     <p>Genre: ${movie.genre}</p>
@@ -82,7 +80,7 @@
                 $("#movie-display").html("");
                 showMovies(movies);
             })
-            .catch(() => $("#movie-display").html("Oops, something went wrong :("));
+            .catch(() => $("#movie-display").html("Oops, something went wrong"));
     });
 
 //Rating sort function for the radio button
@@ -99,7 +97,7 @@
                 $("#movie-display").html("");
                 showMovies(movies);
             })
-            .catch(() => $("#movie-display").html("Oops, something went wrong :("));
+            .catch(() => $("#movie-display").html("Oops, something went wrong"));
     });
 
     //Genre sort function for the radio button
@@ -122,133 +120,67 @@
                 $("#movie-display").html("");
                 showMovies(movies);
             })
-            .catch(() => $("#movie-display").html("Oops, something went wrong :("));
+            .catch(() => $("#movie-display").html("Oops, something went wrong"));
     });
 
-    // let button = document.querySelector('#submit');
-// let input = document.querySelector('#search');
-// button.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     let searchTerm = input.value;
-//     console.log(searchTerm);
-//     fetch(URL)
-//         .then((resp) => resp.json())
-//         .then((movies) => {
-//             //console.log(data)
-//             document.querySelector('#user').innerHTML = `${movies['0']['actor'].login}`,
-//                 document.querySelector('#last-push').innerHTML = `${movies['0']['created_at']}`,
-//                 document.querySelector('#repo').innerHTML = `${movies['0']['repo'].name}`
-//         })
-//         .catch(error => console.error(error));
-// })
 
-
-    let button3 = document.querySelector("#submit")
-    button3.addEventListener('click', function(name) {
-        let divData = document.querySelector("#movie-display");
-        let divList = divData.querySelectorAll("div");
-        let input3 = document.querySelector('#search-text');
-        let searchTerm = input3.value.toLowerCase();
-        for (let x of divList) {
-            if (x.innerText.includes(searchTerm.value)) {
-
-            } else {
-                x.style.backgroundColor = "#999"
-            }
-        }
+    //Search functions - checks title and genre
+    $("#submit").click((e) => {
+        e.preventDefault()
+        let searchBox = document.querySelector('#search')
+        let searchTerm = searchBox.value.toLowerCase()
+        fetch(URL)
+            .then(response => response.json())
+            .then(movies => movies.filter((movie) => movie.title.toLowerCase().includes(searchTerm) || movie.genre.toLowerCase().includes(searchTerm)))
+            .then(movie => showMovies(movie))
+            .catch(() => $("#movie-display").html("Oops, something went wrong"));
     })
-
-//rough draft code below. will need finished forms to be useful
-
-
-//     const addMovie = (movieObj) => {
-//         let options = {
-//             method: "POST",
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(songObj)
-//         }
-//         return fetch(URL, options).then(res => res.json()).then(result => console.log("You've added a movie", result))
-//     }
-
-//add movie click event
-//document.getElementById("ENTERLOCATION").addEventListener("click", function (e) {
-//     e.preventDefault();
-//     let newMovie = {
-//         title: document.getElementById("title").value,
-//         artist: document.getElementById("artist").value
-//     }
-//     addMovie(newMovie).then((res) => {
-//         console.log(res)
-//         buildMovie()
-//     })
-// })
-
-
-// Edit Movies
-//     const changeMovie = (movie) => {
-//         let options = {
-//             method: "PATCH",
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             },
-//             body: JSON.stringify(movie)
-//         }
-//         return fetch(`${URL}/${song.id}`, options).then(resp => resp.json())
-//     }
-//     let updatedMovie = {
-//         id: 1,
-//         title: "TNT",
-//     }
-//changeMovie(updatedMovie);
-
 
 
 //convert rating in API call to a star display
-    function starRating(num) {
-        if (num === 1) {
-            return `
+function starRating(num) {
+    if (num === 1) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-        } else if (num === 2) {
-            return `
+    } else if (num === 2) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-        } else if (num === 3) {
-            return `
+    } else if (num === 3) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                      <span class="fa fa-star"></span>
                     `
-        } else if (num === 4) {
-            return `
+    } else if (num === 4) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star"></span>
                     `
-        } else if (num === 5) {
-            return `
+    } else if (num === 5) {
+        return `
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                      <span class="fa fa-star checked"></span>
                     `
-        }
     }
+}
 
 
 }())
